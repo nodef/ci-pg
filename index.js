441181c5aed938ee4d76352cc50a1e152acc7bd0
Subhajit Sahu <wolfram77@gmail.com>
2018-01-05 21:03:13 +0530

    v0.0.38

--- index.js
+++ index.js
@@ -3,19 +3,27 @@ var pool = require('./pool');
 
 var app = process.env.HEROKU_APP_NAME;
 var port = process.env.PORT||80;
+var server = http.createServer().listen(port);
 
-var server = function() {
-  http.createServer(function(req, res) {
+pool.setup(app).then((ans) => {
+  server.on('request', (req, res) => {
+    var id = req.socket.fd;
+    console.log(`server.request(${id})`);
     res.writeHead(200, {
       'Content-Type': 'text/plain',
       'Transfer-Encoding': 'chunked',
       'X-Content-Type-Options': 'nosniff'
     });
-    res.write('Hello World!');
-  }).listen(port);
-};
-
-pool.setup(app).then((ans) => {
-  console.log(ans);
-  server();
+    pool.remove(id).then((ans) => {
+      var val = pool.get(ans);
+      console.log(`server.request:poolRemove(${val})`);
+      res.write(val);
+    });
+    var onend = function() {
+      console.log(`server.request:end(${id})`);
+      pool.add(id);
+    };
+    req.on('aborted', onend);
+    req.on('close', onend);
+  });
 });

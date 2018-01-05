3718be2e20409bd31f6f857e0ee770a22b810dcf
Subhajit Sahu <wolfram77@gmail.com>
2018-01-05 21:01:25 +0530

    v0.0.31

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var http = require('http');
 var cp = require('child_process');
 
 var app = process.env.HEROKU_APP_NAME;
-console.log(cp.execSync('./heroku --version'));
+console.log(cp.execSync('./heroku --version').toString());
 http.createServer(function(req, res) {
-  console.log(cp.execSync('./heroku --version'));
+  console.log(cp.execSync('./heroku --version').toString());
   res.writeHead(200, {
     'Content-Type': 'text/plain',
     'Transfer-Encoding': 'chunked',

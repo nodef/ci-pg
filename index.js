ed60ef63622c4b5d6c2381f7bd2a0a0162d27fbb
Subhajit Sahu <wolfram77@gmail.com>
2018-01-05 20:55:24 +0530

    v0.0.5

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var http = require('http');
 
 console.log(process.env.HEROKU_APP_NAME);
 http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, {
+    'Content-Type': 'text/plain',
+    'Transfer-Encoding': 'chunked',
+    'X-Content-Type-Options': 'nosniff'
+  });
   res.write('Hello World!');
 }).listen(process.env.PORT);

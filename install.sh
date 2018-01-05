14064ce5b2f0414bc8a8e9779f35d22c727567ae
Subhajit Sahu <wolfram77@gmail.com>
2018-01-05 20:58:58 +0530

    v0.0.20

--- install.sh
+++ install.sh
@@ -4,17 +4,25 @@ wget ${HEROKU_CLI_URL} -O heroku.tar.gz
 tar -xvzf heroku.tar.gz
 popd
 echo "-------"
+echo tmp:
 ls tmp
 echo "-------"
+echo .:
 ls
 echo "-------"
+echo tmp:
 ls -d tmp
 echo "-------"
 mv $(ls -d tmp) lib/heroku
 echo "-------"
+echo lib/heroku
 ls lib/heroku
+echo "-------"
+echo lib/heroku/bin:
+ls lib/heroku/bin
 echo "-------"
 ln -s $PWD/lib/heroku/bin/heroku $PWD/heroku
 echo "-------"
+echo .:
 ls
 echo "-------"

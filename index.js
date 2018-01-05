426ad17e4088ccbdb6b848895767897821a1326c
Subhajit Sahu <wolfram77@gmail.com>
2018-01-05 20:55:10 +0530

    v0.0.4

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var http = require('http');
 
+console.log(process.env.HEROKU_APP_NAME);
 http.createServer(function(req, res) {
-  console.log(process.env);
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('Hello World!');
-  res.end();
 }).listen(process.env.PORT);

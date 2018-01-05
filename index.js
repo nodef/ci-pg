7dd86092665e59a09c831adfab20298b382762a4
Subhajit Sahu <wolfram77@gmail.com>
2018-01-05 20:59:45 +0530

    v0.0.23

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ var http = require('http');
 var cp = require('child_process');
 
 var app = process.env.HEROKU_APP_NAME;
-cp.execSync('./heroku --version', {
-  'cwd': __dirname
-});
+cp.execSync('./heroku --version');
 cp.execSync('./heroku login', {
-  'cwd': __dirname,
   'input': `${HEROKU_EMAIL}\n${HEROKU_PASSWORD}\n`
 });
 http.createServer(function(req, res) {
--- install.sh
+++ install.sh
@@ -4,5 +4,4 @@ wget ${HEROKU_CLI_URL} -O heroku.tar.gz
 tar -xvzf heroku.tar.gz
 mv $(ls|head -n 1) heroku
 popd
-ln -s $PWD/lib/heroku/bin/heroku $PWD/heroku
 chmod 777 heroku
